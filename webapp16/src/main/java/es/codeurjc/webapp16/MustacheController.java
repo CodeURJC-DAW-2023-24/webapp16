34cd65167f5b449e44221cbf70ed6f3af43dcc03
nicohht <n.hernandezt.2021@alumnos.urjc.es>
2024-02-19 12:31:42 +0100

    fixed: bug path

--- webapp16/src/main/java/es/codeurjc/webapp16/MustacheController.java
+++ webapp16/src/main/java/es/codeurjc/webapp16/MustacheController.java
@@ -8,10 +8,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class MustacheController {
 	
-	@GetMapping("/tournament_bracket")
-	public String tour(Model model) {
-		return "tournament_bracket";
-	}
     @GetMapping("/header")
 	public String haed(Model model) {
 		return "header";
