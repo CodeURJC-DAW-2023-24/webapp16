7f6417e5b98942f9345b77415562840aea4e3171
nicohht <n.hernandezt.2021@alumnos.urjc.es>
2024-03-04 10:29:49 +0100

    Delete: old files

--- backend/src/main/java/es/codeurjc/backend/controller/CustomErrorController.java
+++ backend/src/main/java/es/codeurjc/backend/controller/CustomErrorController.java
@@ -1,11 +1,9 @@
 package es.codeurjc.backend.controller;
 
-import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.context.request.WebRequest;
 
 @Controller
 public class CustomErrorController implements ErrorController {
@@ -15,13 +13,13 @@ public class CustomErrorController implements ErrorController {
         int statusCode = response.getStatus();
 
         if (statusCode == 404) {
-            return "not-found";  // Página de error personalizada para error 404
+            return "notFound";  // Página de error personalizada para error 404
         } else if (statusCode == 403) {
-            return "access-denied";  // Página de error personalizada para error 403
+            return "accessDenied";  // Página de error personalizada para error 403
         }
 
         // En otros casos, redirige a una página de error general
-        return "not-found";
+        return "notFound";
     }
 }
 
