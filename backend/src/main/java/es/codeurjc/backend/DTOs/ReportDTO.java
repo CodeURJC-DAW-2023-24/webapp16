c54ec67dc61b1cc8edb14db3fe8122d3a5046efb
nataaah <n.hernandezv.2021@alumnos.urjc.es>
2024-03-28 10:48:33 +0100

    Getters and Setters ReportDTO.java

--- backend/src/main/java/es/codeurjc/backend/DTOs/ReportDTO.java
+++ backend/src/main/java/es/codeurjc/backend/DTOs/ReportDTO.java
@@ -12,11 +12,11 @@ public class ReportDTO {
     private String observations;
     private Matches match;
 
-    //Constructor vacio
+    //Empty Constructor
     public ReportDTO(){
     }
 
-    //Constructor con parametros
+    //Constructor with parameters
     public ReportDTO(Long id, String date, String time, String matchOfficials, int localTeamGoals, int visitingTeamGoals, String observations, Matches match){
         this.id = id;
         this.date = date;
@@ -27,4 +27,20 @@ public class ReportDTO {
         this.observations = observations;
         this.match = match;
     }
+
+    //Getters and Setters
+    public Long getId() {return id;}
+    public void setId(Long id) {this.id = id;}
+    public String getDate() {return date;}
+    public void setDate(String date) {this.date = date;}
+    public String getTime() {return time;}
+    public void setTime(String time) {this.time = time;}
+    public int getLocalTeamGoals() {return localTeamGoals;}
+    public void setLocalTeamGoals(int localTeamGoals) {this.localTeamGoals = localTeamGoals;}
+    public int getVisitingTeamGoals() {return visitingTeamGoals;}
+    public void setVisitingTeamGoals(int visitingTeamGoals) {this.visitingTeamGoals = visitingTeamGoals;}
+    public String getObservations() {return observations;}
+    public void setObservations(String observations) {this.observations = observations;}
+    public Matches getMatch() {return match;}
+    public void setMatch(Matches match) {this.match = match;}
 }
