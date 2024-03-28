e5ed80ca546082d340cc27a702e198e230cb571e
nataaah <n.hernandezv.2021@alumnos.urjc.es>
2024-03-28 10:30:51 +0100

    Constructors ReportDTO.java

--- backend/src/main/java/es/codeurjc/backend/DTOs/ReportDTO.java
+++ backend/src/main/java/es/codeurjc/backend/DTOs/ReportDTO.java
@@ -1,4 +1,30 @@
 package es.codeurjc.backend.DTOs;
 
+import es.codeurjc.backend.model.Matches;
+
 public class ReportDTO {
+    private Long id;
+    private String date;
+    private String time;
+    private String matchOfficials;
+    private int localTeamGoals;
+    private int visitingTeamGoals;
+    private String observations;
+    private Matches match;
+
+    //Constructor vacio
+    public ReportDTO(){
+    }
+
+    //Constructor con parametros
+    public ReportDTO(Long id, String date, String time, String matchOfficials, int localTeamGoals, int visitingTeamGoals, String observations, Matches match){
+        this.id = id;
+        this.date = date;
+        this.time = time;
+        this.matchOfficials = matchOfficials;
+        this.localTeamGoals = localTeamGoals;
+        this.visitingTeamGoals = visitingTeamGoals;
+        this.observations = observations;
+        this.match = match;
+    }
 }
