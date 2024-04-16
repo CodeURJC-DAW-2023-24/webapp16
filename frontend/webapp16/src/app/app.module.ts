0a1bb01ece50907df9ffdf5349abe2fe66e3b85c
aadrisoriiano <aadrisoriiano@gmail.com>
2024-04-16 18:18:43 +0200

    ADD : header and footer components

--- frontend/webapp16/src/app/app.module.ts
+++ frontend/webapp16/src/app/app.module.ts
@@ -4,10 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './components/app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import {FooterComponent} from "./components/generalComponents/footerComponent/footer.component";
+import {HeaderComponent} from "./components/generalComponents/headerComponent/header.component";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
