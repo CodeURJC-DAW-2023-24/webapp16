33187a68c2cf3712650dbd101c4010d67feea0d5
Ahmad-SE <a.abdel.2021@alumnos.urjc.es>
2024-05-04 21:44:27 +0200

    Add: ShowReportComponent, FillMatchReport logic

--- frontend/webapp16/src/app/components/viewsComponents/fillMatchReportComponent/fillMatchReport.component.ts
+++ frontend/webapp16/src/app/components/viewsComponents/fillMatchReportComponent/fillMatchReport.component.ts
@@ -1,5 +1,8 @@
 import {Component, OnInit, ViewEncapsulation} from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import {MatchService} from "../../../services/match.service";
+import {ActivatedRoute, Router} from "@angular/router";
+import {throwError} from "rxjs";
 
 @Component({
   selector: 'app-fill-match-report',
@@ -9,20 +12,70 @@ import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 })
 export class FillMatchReportComponent implements OnInit {
   reportForm!: FormGroup;
-  constructor(private fb: FormBuilder) {}
+  report: any;
+  matchId: any;
+  match: any;
+  constructor(private fb: FormBuilder, private matchService: MatchService, private route: ActivatedRoute, private router: Router) {
+    this.reportForm = this.fb.group({
+      date: ['', Validators.required],
+      time: ['', Validators.required],
+      localTeamGoals: ['', [Validators.required, Validators.min(0)]],
+      visitingTeamGoals: ['', [Validators.required, Validators.min(0)]],
+      matchOfficials: ['', Validators.required],
+      observations: ['', Validators.required],
+      localTeam: ['', Validators.required],
+      visitingTeam: ['', Validators.required]
+    });
+  }
 
   ngOnInit() {
-    this.reportForm = this.fb.group({
-      dateOfBirth: ['', Validators.required],
-      matchTime: ['', Validators.required],
-      // otros campos...
-    });
+    const Id = this.route.snapshot.paramMap.get('id');
+    if (Id !== null) {
+      this.matchService.getMatchesById(Id).subscribe(
+        match => {
+          this.matchId = match.id;
+          this.match = match;
+          this.reportForm.patchValue({
+            localTeam: {value: this.match.localTeam.name, disabled: true},
+            visitingTeam: {value: this.match.visitingTeam.name, disabled: true},
+          });
+        },
+        error => {
+          const errorCode = error.status;
+          this.router.navigate(['/error'], { state: { errorCode: errorCode } });
+          return throwError(error);
+        }
+      );
+    } else {
+      throw new Error("Match ID is null");
+    }
   }
 
   onSubmit() {
     if (this.reportForm.valid) {
-      console.log(this.reportForm.value);
-      // lógica de envío aquí
+      const report = {
+        date: this.reportForm.value.date,
+        time: this.reportForm.value.time,
+        matchOfficials: this.reportForm.value.matchOfficials,
+        localTeamGoals: this.reportForm.value.localTeamGoals,
+        visitingTeamGoals: this.reportForm.value.visitingTeamGoals,
+        observations: this.reportForm.value.observations,
+        match: this.match,
+      };
+      console.log(report);
+      this.matchService.postMatchReport(report).subscribe(
+        response => {
+          console.log(response);
+          // lógica de éxito aquí
+        },
+        error => {
+          console.log(error);
+          // lógica de error aquí
+        }
+      );
+    }
+    else{
+      console.log("Invalid form");
     }
   }
 }
--- frontend/webapp16/src/app/components/viewsComponents/showReportComponent/showReport.component.ts
+++ frontend/webapp16/src/app/components/viewsComponents/showReportComponent/showReport.component.ts
@@ -1,5 +1,8 @@
+// En showReport.component.ts
 import {Component, OnInit, ViewEncapsulation} from '@angular/core';
 import {Title} from "@angular/platform-browser";
+import {ActivatedRoute} from '@angular/router';
+import {MatchService} from '../../../services/match.service';
 
 @Component({
   selector: 'app-show-report',
@@ -11,10 +14,19 @@ export class ShowReportComponent implements OnInit {
   error: string | null = null;
   report: any;
 
-  constructor(private titleService: Title) {}
+  constructor(private titleService: Title, private matchService: MatchService, private route: ActivatedRoute) {}
 
   ngOnInit() {
-    this.titleService.setTitle('Report page');
+    const id = this.route.snapshot.paramMap.get('id');
+    if (id != null) {
+      this.titleService.setTitle('Report page');
+      this.matchService.getReportById(id).subscribe(
+        (report: Report) => {
+          this.report = report;
+          console.log(this.report);
+        }
+      );
+    }
   }
 
 }
