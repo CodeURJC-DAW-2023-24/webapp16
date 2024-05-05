02e230f37178147ad0d556371b54cca6357a2de0
nicohht <n.hernandezt.2021@alumnos.urjc.es>
2024-05-05 22:58:55 +0200

    Add: hide report if not exist

--- frontend/webapp16/src/app/components/viewsComponents/matchInfoComponent/matchInfo.component.ts
+++ frontend/webapp16/src/app/components/viewsComponents/matchInfoComponent/matchInfo.component.ts
@@ -4,6 +4,8 @@ import {Observable, throwError} from "rxjs";
 import {Title} from "@angular/platform-browser";
 import {ActivatedRoute, Router} from "@angular/router";
 import {AuthService} from "../../../services/auth.service";
+import {ReportsService} from "../../../services/reports.service";
+
 @Component({
   selector: 'matchInfo',
   templateUrl: './matchInfo.component.html',
@@ -14,25 +16,33 @@ export class MatchInfoComponent implements OnInit{
   match: any;
   matchId: any;
   isAdmin$: Observable<boolean>;
+  hasReport: boolean = false;
 
-  constructor(public authService: AuthService,private titleService: Title, private route: ActivatedRoute, private router: Router, private matchService: MatchService) {
+  constructor(public authService: AuthService,private titleService: Title, private route: ActivatedRoute, private router: Router, private matchService: MatchService, private reportService: ReportsService) {
     this.isAdmin$ = this.authService.isAdmin();
   }
   ngOnInit(): void {
+
     this.isAdmin$ = this.authService.isAdmin();
     const id = this.route.snapshot.paramMap.get('id'); //get match ID from URL
     this.matchId= id;
+    //this.hasReport = !!this.match.report;
 
     this.titleService.setTitle('matchInfo/' + id);
     if (id != null) {
       this.matchService.getMatchesById(id).subscribe({
         next : (match) => {
           this.match = match;
+          console.log(this.matchId)
+          this.reportService.getReportById(this.matchId).subscribe((report: any) => {
+            console.log("This is the report object: " + !!report);
+            this.hasReport = !!report; // Set hasReport to true if report exists
+          });
           //console.log("This is the match object: " + JSON.stringify(match));
           },
         error: (error) => {
           const errorCode = error.status;
-          this.router.navigate(['/error'], { state: { errorCode: errorCode } });
+          this.router.navigate(['/login'], { state: { errorCode: errorCode } });
           return throwError(error);
         }
       }
