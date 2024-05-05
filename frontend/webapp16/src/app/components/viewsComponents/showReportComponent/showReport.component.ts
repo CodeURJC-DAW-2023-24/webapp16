0a2d4e7e93ce892c4783083d35ecf201c7f22ebf
aadrisoriiano <aadrisoriiano@gmail.com>
2024-05-05 22:48:44 +0200

    ADD: PDF download
    !semi-important

--- frontend/webapp16/src/app/components/viewsComponents/showReportComponent/showReport.component.ts
+++ frontend/webapp16/src/app/components/viewsComponents/showReportComponent/showReport.component.ts
@@ -4,6 +4,8 @@ import {Title} from "@angular/platform-browser";
 import {ActivatedRoute} from '@angular/router';
 import {MatchService} from '../../../services/match.service';
 import {ReportsService} from "../../../services/reports.service";
+import { jsPDF } from 'jspdf';
+import html2canvas from 'html2canvas';
 
 @Component({
   selector: 'app-show-report',
@@ -30,4 +32,25 @@ export class ShowReportComponent implements OnInit {
     }
   }
 
+ genPDF() {
+  let data = document.getElementById('allInfo');  // id of the form
+  if (data) {
+    html2canvas(data).then(canvas => {
+      // Few necessary setting options
+      let imgWidth = 208;
+      let pageHeight = 295;
+      let imgHeight = canvas.height * imgWidth / canvas.width;
+      let heightLeft = imgHeight;
+
+      const contentDataURL = canvas.toDataURL('image/png')
+      let pdf = new jsPDF('p', 'mm', 'a4'); // A4 size page of PDF
+      let position = 0;
+      pdf.addImage(contentDataURL, 'PNG', 0, position, imgWidth, imgHeight)
+      let name = 'ReportMatch/' + this.report.match.id;
+      pdf.save(name); // Generated PDF
+    });
+  } else {
+    console.error('Element with id "allInfo" not found');
+  }
+}
 }
